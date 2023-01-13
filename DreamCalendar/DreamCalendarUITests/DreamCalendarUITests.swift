51ff1a89f7641d558a69b01995c7f988e9485670
JISU LEE <wltn102998@gmail.com>
2023-01-13 17:11:39 +0900

    #8 [STYLE] final 키워드 적용

--- DreamCalendar/DreamCalendarUITests/DreamCalendarUITests.swift
+++ DreamCalendar/DreamCalendarUITests/DreamCalendarUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class DreamCalendarUITests: XCTestCase {
+final class DreamCalendarUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
