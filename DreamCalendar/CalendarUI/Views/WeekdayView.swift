6d086e80ad8ee958cb221e4d87cf3264ba77342d
JISU LEE <wltn102998@gmail.com>
2023-01-23 14:57:41 +0900

    #8 [FEAT] 요일, 날짜, 블록 간격 조정

--- DreamCalendar/CalendarUI/Views/WeekdayView.swift
+++ DreamCalendar/CalendarUI/Views/WeekdayView.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 
 struct WeekdayView: View {
+    
+    private struct Constraint {
+        static let height: CGFloat = 13
+        static let width: CGFloat = 46
+        static let leadingTrailingPadding: CGFloat = 10
+        static let topPadding: CGFloat = 7
+        static let bottomPadding: CGFloat = 6
+        static let zeroPadding: CGFloat = 0
+        static let blockInterval: CGFloat = 2
+    }
+    
     var body: some View {
-        HStack {
-            Spacer()
+        HStack(alignment: .center, spacing: Constraint.blockInterval) {
             ForEach(Days.allCases, id: \.hashValue) { day in
                 Text(String(describing: day))
                     .foregroundColor(Color.dayGray)
                     .font(Font.AppleSDSemiBold12)
-                    .frame(maxWidth: .infinity, minHeight: 13, maxHeight: 13, alignment: .center)
+                    .frame(width: Constraint.width,
+                           height: Constraint.height,
+                           alignment: .center)
             }
-            Spacer()
         }
-        .frame(height: 13, alignment: .bottom)
-        .padding(EdgeInsets(top: 7, leading: 0, bottom: 6, trailing: 0))
+        .frame(height: Constraint.height, alignment: .bottom)
+        .padding(EdgeInsets(top: Constraint.topPadding,
+                            leading: Constraint.leadingTrailingPadding,
+                            bottom: Constraint.bottomPadding,
+                            trailing: Constraint.leadingTrailingPadding))
     }
 }
