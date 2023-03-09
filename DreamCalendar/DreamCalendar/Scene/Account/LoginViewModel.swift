6a32eef71a7910fee33ba26f925907e82daf0bbe
JISU LEE <wltn102998@gmail.com>
2023-03-09 15:06:18 +0900

    Revert "[계정] 로그인 화면 재구성"

--- DreamCalendar/DreamCalendar/Scene/Account/LoginViewModel.swift
+++ DreamCalendar/DreamCalendar/Scene/Account/LoginViewModel.swift
@@ -64,6 +64,6 @@ final class LoginViewModel: ObservableObject {
         self.error = nil
         self.didError = false
         self.loginMessage = nil
-        self.didLogin = nil
+        self.didLogin = false
     }
 }
